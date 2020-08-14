8ced740cf0ac9e14ba2bea7f84b6a3044163c330
Donald Lee <51865552+Donald-K-Lee@users.noreply.github.com>
2020-08-13 22:10:40 -0700

    Bot can now send messages to multiple recipients

--- InstagramDMBot.py
+++ InstagramDMBot.py
@@ -8,15 +8,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
-myemail = ""            # Enter in your email, phone number, or username for Instagram inside the quotation marks
-mypassword = ""         # Enter in your password for Instagram inside the quotation marks
-friendusername = ""     # Enter in the username of the recipient inside the quotation marks
-numoftimes = ""         # Enter in the number of times you would like to send the message to the recipient Ex) 1
+myemail = ""                # Enter in your email for Instagram inside the quotation marks
+mypassword = ""             # Enter in your password for Instagram inside the quotation marks
+friendusernames = [""]      # Enter in the username of the recipient inside the quotation marks! To send this to mutiple users, seperate users by commas! Ex)  friendusernames = ["@friend1", "@friend2", "@friend3"]
+numoftimes = ""             # Enter in the number of times you would like to send the message to the recipient Ex) "2"
 
 # The message being sent
 message = ""
 
-PATH = ""  # Step 4 of the installations instructions
+PATH = ""                   # Step 4 of the installations instructions
 
 driver = webdriver.Chrome(PATH)
 
@@ -49,54 +49,51 @@ try:
     except:
         print ("Could not click not now on the notifications pop up!")
 
-    try:
-        searchuser = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.EQ1Mr')))
-        searchuser.click()
-    except:
-        print ("Could not click on the new message button!")
+    for friendusername in friendusernames:
+        try:
+            searchuser = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.EQ1Mr')))
+            searchuser.click()
+        except:
+            print ("Could not click on the new message button!")
+        try:
+            searchuserbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.j_2Hd')))
+            searchuserbox.send_keys(friendusername)
+        except:
+            print ("Could not find the enter username box!")
 
-    try:
-        searchuserbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.j_2Hd')))
-        searchuserbox.send_keys(friendusername)
-    except:
-        print ("Could not find the enter username box!")
+        try:
+            time.sleep(2)
+            firstuser = driver.find_element_by_css_selector('.HVWg4')
+            firstuser.click()
+        except:
+            print("Could not click on the first user!")
 
-    try:
-        time.sleep(2)
-        firstuser = driver.find_element_by_css_selector('.HVWg4')
-        firstuser.click()
-    except:
-        print("Could not click on the first user!")
+        try:
+            pressingnext = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rIacr')))
+            pressingnext.click()
+        except:
+            print ("Could not press \"Next\"!")
 
-    try:
-        pressingnext = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.rIacr')))
-        pressingnext.click()
-    except:
-        print ("Could not press \"Next\"!")
+        try:
+            messagebox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.focus-visible')))
+            messagebox.click()
+        except:
+            print ("Could not find the text box!")
 
-    try:
-        messagebox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.focus-visible')))
-        messagebox.click()
-    except:
-        print ("Could not find the text box!")
+        try:
+            # Finds the text box
+            textbox = driver.find_element_by_css_selector('.focus-visible')
+        except:
+            print("Could not find the text box!")
 
-    try:
-        # Finds the text box
-        textbox = driver.find_element_by_css_selector('.focus-visible')
-    except:
-        print("Could not find the text box!")
+        try:
+            # Sends the message
+            for i in range(int(numoftimes)):
+                textbox.send_keys(message)
+                textbox.send_keys(Keys.RETURN)
+        except:
+            print("Error sending the message!")
 
-    try:
-        # Sends the message
-        for i in range(int(numoftimes)):
-            textbox.send_keys(message)
-            textbox.send_keys(Keys.RETURN)
-        print('Your message(s) have been sent!')
-    except:
-        print("Error sending the message!")
-
-    time.sleep(3)
-    driver.quit()
 except:
     print("An error has occurred")
     time.sleep(1)
